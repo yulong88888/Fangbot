c94da563bb252b072e171e5f1ab284c0acb0b83c
YuLong <1327417509@qq.com>
2019-05-31 14:31:57 +0800

    加入管脚注释

--- src/config.h
+++ src/config.h
@@ -3,22 +3,25 @@
 
 #include "ShiftStepper.h"
 
-//
+//电机
 #define SHIFT_REG_DATA 14
 #define SHIFT_REG_CLOCK 15
 #define SHIFT_REG_LATCH 16
 
+//蓝牙串口
 #define SOFT_SERIAL_BLE_T 4
 #define SOFT_SERIAL_BLE_R 5
 
+//MP3串口
 #define SOFT_SERIAL_MP3_T 6
 #define SOFT_SERIAL_MP3_R 7
 
+//舵机
 #define SERVO 9
 
+//NFC
 #define NFC_SS_PIN 10
 
-//
 #define STEPS_PER_TURN 2048.0f
 
 #define CIRCUMFERENCE_MM 230.0f
